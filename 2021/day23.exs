325dd12c8b5b827458214846f184e07a6cfbdf34
Po Chen <chenpaul914@gmail.com>
2021-12-24 12:17:55 +1100

    finish day 23

--- 2021/day23.exs
+++ 2021/day23.exs
@@ -1,47 +1,59 @@
+# Title: Day23
+
+# ── Untitled ──
+
 data =
-  """
-  #############
-  #...........#
-  ###B#B#D#A###
-    #C#A#D#C#
-    #########
-  """
+  "input"
+  |> IO.getn(1_000_000)
   |> String.trim()
   |> String.split(["\r\n", "\n"], trim: true)
-  |> then(fn [_, dots, row1, row2, _] ->
-    ((Regex.scan(~r/\./, dots, return: :index)
-      |> Enum.map(fn [{i, _}] -> {{0, i}, nil} end)) ++
-       (Regex.scan(~r/[A-D]/, row1)
+  |> Enum.split(2)
+  |> then(fn {[_, hallway], rooms} ->
+    hallway =
+      (Regex.scan(~r/\./, hallway, return: :index)
+       |> Enum.map(fn [{i, _}] -> {{0, i}, nil} end)) --
+        ([3, 5, 7, 9]
+         |> Enum.map(fn i -> {{0, i}, nil} end))
+
+    rooms =
+      rooms
+      |> Enum.drop(-1)
+      |> Enum.with_index(1)
+      |> Enum.map(fn {rooms, row} ->
+        Regex.scan(~r/[A-D]/, rooms)
         |> Enum.zip([3, 5, 7, 9])
-        |> Enum.map(fn {[x], i} -> {{1, i}, {x, false}} end)) ++
-       (Regex.scan(~r/[A-D]/, row2)
-        |> Enum.zip([3, 5, 7, 9])
-        |> Enum.map(fn {[x], i} -> {{2, i}, {x, false}} end))) --
-      ([3, 5, 7, 9]
-       |> Enum.map(fn i -> {{0, i}, nil} end))
+        |> Enum.map(fn {[type], col} -> {{row, col}, {type, false}} end)
+      end)
+      |> List.update_at(-1, fn rooms ->
+        Enum.map(rooms, fn {{row, col}, {type, false}} ->
+          if {col, type} in [{3, "A"}, {5, "B"}, {7, "C"}, {9, "D"}] do
+            {{row, col}, {type, true}}
+          else
+            {{row, col}, {type, false}}
+          end
+        end)
+      end)
+      |> List.flatten()
+
+    hallway ++ rooms
   end)
   |> Map.new()
-  |> Map.update!({2, 3}, fn {t, _} -> {t, t == "A"} end)
-  |> Map.update!({2, 5}, fn {t, _} -> {t, t == "B"} end)
-  |> Map.update!({2, 7}, fn {t, _} -> {t, t == "C"} end)
-  |> Map.update!({2, 9}, fn {t, _} -> {t, t == "D"} end)
 
 defmodule D23 do
   @weight %{"A" => 1, "B" => 10, "C" => 100, "D" => 1000}
+  @col %{"A" => 3, "B" => 5, "C" => 7, "D" => 9}
 
-  def search(map, cost, seen, path) do
+  def search(map, rc, cost, min_cost, seen) do
     shrimps = Enum.reject(map, &is_nil(elem(&1, 1)))
     shrimps_tuple = shrimps |> Enum.sort() |> List.to_tuple()
     all_done? = Enum.all?(shrimps, &match?({_, {_, true}}, &1))
 
     cond do
       all_done? ->
-        if {map[{2, 3}], map[{2, 5}], map[{2, 7}], map[{2, 9}]} ==
-             {{"A", true}, {"B", true}, {"C", true}, {"D", true}} do
-          {seen, cost}
-        else
-          {seen, :infinity}
-        end
+        {seen, cost}
+
+      cost > min_cost ->
+        {seen, :infinity}
 
       (found = seen[shrimps_tuple]) && elem(found, 0) <= cost ->
         {seen, seen[shrimps_tuple]}
@@ -52,55 +64,47 @@ defmodule D23 do
         out_options =
           shrimps
           |> Enum.filter(fn {{row, col}, {_type, done?}} ->
-            not done? && (row == 1 or (row == 2 && is_nil(map[{1, col}])))
+            not done? && row > 0 &&
+              (row == 1 or
+                 Enum.all?(1..(row - 1)//1, fn r -> !map[{r, col}] end))
           end)
           |> Enum.map(fn {{_row, col} = coords, {type, _done?}} ->
             {{coords, {type, false}},
-             Enum.filter(hallway, fn
+             hallway
+             |> Enum.filter(fn
                {{_, hw_col}, nil} -> Enum.all?(hw_col..col, &(!map[{0, &1}]))
                _ -> false
-             end)}
+             end)
+             |> Enum.map(&elem(&1, 0))}
           end)
 
         in_options =
           shrimps
-          |> Enum.filter(fn {{row, _col}, {_type, _done?}} -> row == 0 end)
-          |> Enum.map(fn {{_row, col} = coords, {type, _done?}} ->
+          |> Enum.filter(fn {{row, col}, {type, _done?}} ->
+            row == 0 &&
+              Enum.count_until(col..@col[type], &map[{0, &1}], 2) == 1 &&
+              Enum.all?(1..rc, &(map[{&1, @col[type]}] in [nil, {type, true}]))
+          end)
+          |> Enum.map(fn {coords, {type, _done?}} ->
             {{coords, {type, true}},
-             [3, 5, 7, 9]
-             |> Enum.map(fn rm_col ->
-               deep = map[{2, rm_col}]
-               shallow = map[{1, rm_col}]
-
-               if is_nil(deep) do
-                 {2, rm_col}
-               else
-                 if elem(deep, 0) == type and is_nil(shallow) do
-                   {1, rm_col}
-                 end
-               end
-               |> then(fn
-                 nil ->
-                   false
-
-                 other ->
-                   Enum.count_until(rm_col..col, &map[{0, &1}], 2) == 1 && {other, nil}
-               end)
-             end)
-             |> Enum.filter(& &1)}
+             rc..1//-1
+             |> Enum.map(&{&1, @col[type]})
+             |> Enum.drop_while(&map[&1])
+             |> Enum.take(1)}
           end)
 
         (out_options ++ in_options)
-        |> Stream.flat_map(fn {s, l} -> Stream.map(l, fn x -> {s, x} end) end)
-        |> Enum.reduce({seen, :infinity}, fn
-          {{{from_row, from_col} = from, {type, _done?} = shrimp}, {{to_row, to_col} = to, _}},
+        |> Enum.flat_map(fn {s, l} -> Stream.map(l, fn x -> {s, x} end) end)
+        |> Enum.reduce({seen, min_cost}, fn
+          {{{from_row, from_col} = from, {type, _done?} = shrimp}, {to_row, to_col} = to},
           {seen, min_cost} ->
             {seen, route_cost} =
               search(
                 map |> Map.put(from, nil) |> Map.put(to, shrimp),
+                rc,
                 @weight[type] * (abs(from_row - to_row) + abs(from_col - to_col)) + cost,
-                seen,
-                [{type, from, to} | path]
+                min_cost,
+                seen
               )
 
             {seen, min(min_cost, route_cost)}
@@ -112,17 +116,10 @@ defmodule D23 do
   end
 end
 
-map = data
+# ── P1 ── (⎇ from Untitled)
 
-for row <- 0..2 do
-  for col <- 1..11 do
-    case Map.get(map, {row, col}, " ") do
-      {type, _} -> type
-      nil -> "."
-      " " -> "_"
-    end
-  end
-  |> IO.puts()
-end
+# D23.search(data, 2, 0, :infinity, %{}) |> elem(1)
 
-IO.inspect(D23.search(map, 0, %{}, []) |> elem(1))
+# ── P2 ── (⎇ from Untitled)
+
+# D23.search(data, 4, 0, :infinity, %{}) |> elem(1)
